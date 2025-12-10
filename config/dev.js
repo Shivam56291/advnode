457f77f6135a6abb7cf49095bf5f4196c211bee6
Shivam Kumar <roysinghaniashivam@gmail.com>
2025-12-10 11:51:21 +0530

    initialize git with initial files

---
+++ config/dev.js
@@ -0,0 +1,15 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb+srv://roysinghaniashivam_db_user:Shivam@143@advanced-nodejs.hqu77gw.mongodb.net/advnode?retryWrites=true&appName=advanced-nodejs",
+  cookieKey: "123123123",
+};
+
+//mongodb+srv://roysinghaniashivam_db_user:Shivam@143@advanced-nodejs.hqu77gw.mongodb.net/?appName=advanced-nodejs
+
+// pw: Shivam@143
+// roysinghaniashivam_db_user
+
+// old one :  mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority
